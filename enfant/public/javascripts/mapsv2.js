88ce2c11b84490c6b4bd3a1607bdc7d289ba8dfe
maud laurent <xyhia.chi@gmail.com>
2020-03-24 10:46:12 +0100

    cleaning folder, add websocket server (parent folder), and child webserver with map, weather and alerting

--- enfant/public/javascripts/mapsv2.js
+++ enfant/public/javascripts/mapsv2.js
@@ -1,92 +1,73 @@
 var mymap = L.map('mapid').setView([43.604652, 1.444209], 13);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicGVkaWJ1cy1tMiIsImEiOiJjazd5a3djMTIwMmZvM3RwNHNuZGpxaHB6In0.cM0yMBv50KSmwpVfhYfp3A', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    minZoom: 12,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  minZoom: 12,
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
 
-//https://api.mapbox.com/direction/v5/mapbox/walking/{x];{y}?access_token=
-// var marker = L.marker([43.604652, 1.444209]).addTo(mymap);
-// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-
-// var popup = L.popup();
-
-// function onMapClick(e) {
-//     popup
-//     .setLatLng(e.latlng)
-//     .setContent("You clicked the map at " + e.latlng.toString())
-//     .openOn(mymap);}
-// mymap.on('click', onMapClick);
+// Liste des waypoints
+var w1 = L.marker([43.608876, 1.445496]).setZIndexOffset(1000).addTo(mymap);
+w1.bindPopup("<b>Jeanne d'arc</b>").openPopup();
+var w2 = L.marker([43.61085, 1.442728]).setZIndexOffset(1000).addTo(mymap);
+w2.bindPopup("<b>Concorde</b>").openPopup();
+var w3 = L.marker([43.61172, 1.438587]).setZIndexOffset(1000).addTo(mymap);
+w3.bindPopup("<b>A. Bernard</b>").openPopup();
+var w4 = L.marker([43.615837, 1.437299]).setZIndexOffset(1000).addTo(mymap);
+w4.bindPopup("<b>Pont des minimes</b>").openPopup();
+var w5 = L.marker([43.618042, 1.43657]).setZIndexOffset(1000).addTo(mymap);
+w5.bindPopup("<b>Eglise des minimes</b>").openPopup();
+var w6 = L.marker([43.623308, 1.434424]).setZIndexOffset(1000).addTo(mymap);
+w6.bindPopup("<b>Banqué</b>").openPopup();
+var w7 = L.marker([43.62685, 1.433694]).setZIndexOffset(1000).addTo(mymap);
+w7.bindPopup("<b>Barrière de paris</b>").openPopup();
+var w8 = L.marker([43.630391, 1.432772]).setZIndexOffset(1000).addTo(mymap);
+w8.bindPopup("<b>Charlas</b>").openPopup();
+var w9 = L.marker([43.632736, 1.43157]).setZIndexOffset(1000).addTo(mymap);
+w9.bindPopup("<b>Jules ferry</b>").openPopup();
+var w10 = L.marker([43.621677, 1.435357]).setZIndexOffset(1000).addTo(mymap);
+w10.bindPopup("<b>Minimes claude nougaro</b>").openPopup();
 
 //ROUTE pour les chemins 
 var controlRoute = L.Routing.control({
-    router: L.Routing.mapbox('pk.eyJ1IjoicGVkaWJ1cy1tMiIsImEiOiJjazd5a3djMTIwMmZvM3RwNHNuZGpxaHB6In0.cM0yMBv50KSmwpVfhYfp3A',{profile:'mapbox/walking'} ),
-    // waypoints: [
-    //   L.latLng(43.608876, 1.445496),
-    //   L.latLng(43.61085, 1.442728),
-    //   L.latLng(43.61172, 1.438587),
-    //   L.latLng(43.615837, 1.437299),
-    //   L.latLng(43.618042, 1.43657),
-    //   L.latLng(43.621677, 1.435357),
-    //   L.latLng(43.623308, 1.434424),
-    //   L.latLng(43.62685, 1.433694),
-    //   L.latLng(43.630391, 1.432772),
-    //   L.latLng(43.632736, 1.43157),
-    // ],
-    waypoints: [
-      {latLng: L.latLng(43.608876, 1.445496), name: "Jeanne d'arc"},
-      {latLng: L.latLng(43.61085, 1.442728), name: "Concorde"},
-      {latLng: L.latLng(43.61172, 1.438587), name: "A. Bernard" },
-      {latLng: L.latLng(43.615837, 1.437299), name: "Pont des minimes"},
-      {latLng: L.latLng(43.618042, 1.43657), name: "Eglise des minimes"},
-      {latLng: L.latLng(43.621677, 1.435357), name: "Minimes claude nougaro"},
-      {latLng: L.latLng(43.623308, 1.434424), name: "Banqué"},
-      {latLng: L.latLng(43.62685, 1.433694), name: "Barrière de paris"},
-      {latLng: L.latLng(43.630391, 1.432772), name: "Charlas"},
-      {latLng: L.latLng(43.632736, 1.43157), name: "Jules ferry"}
-      // L.Routing.Waypoint(L.latLng(43.608876, 1.445496), "TOTO"),
-      // L.Routing.Waypoint(L.latLng(43.61085, 1.442728), "TOTO")
+  router: L.Routing.mapbox('pk.eyJ1IjoicGVkaWJ1cy1tMiIsImEiOiJjazd5a3djMTIwMmZvM3RwNHNuZGpxaHB6In0.cM0yMBv50KSmwpVfhYfp3A', { profile: 'mapbox/walking' }),
+  waypoints: [
+    { latLng: L.latLng(43.608876, 1.445496), name: "Jeanne d'arc" },
+    { latLng: L.latLng(43.61085, 1.442728), name: "Concorde" },
+    { latLng: L.latLng(43.61172, 1.438587), name: "A. Bernard" },
+    { latLng: L.latLng(43.615837, 1.437299), name: "Pont des minimes" },
+    { latLng: L.latLng(43.618042, 1.43657), name: "Eglise des minimes" },
+    { latLng: L.latLng(43.621677, 1.435357), name: "Minimes claude nougaro" },
+    { latLng: L.latLng(43.623308, 1.434424), name: "Banqué" },
+    { latLng: L.latLng(43.62685, 1.433694), name: "Barrière de paris" },
+    { latLng: L.latLng(43.630391, 1.432772), name: "Charlas" },
+    { latLng: L.latLng(43.632736, 1.43157), name: "Jules ferry" }
+  ],
+  language: 'fr',
+  draggableWaypoints: false,
+  addWaypoints: false,
+  summaryTemplate: '<h2>Itinéraire Jeanne d\'arc <-> Ecole élémentaire Jules ferry</h2><h3>{distance}, {time}</h3>'
+}).addTo(mymap);
+L.control.locate().addTo(mymap);
 
-    ],
-    language:'fr',
-    draggableWaypoints: false,
-    addWaypoints: false,
-    summaryTemplate: '<h2>Itinéraire Jeanne d\'arc <-> Ecole élémentaire Jules ferry</h2><h3>{distance}, {time}</h3>'
-   
-  })
-  .addTo(mymap);
+// websocket connection
+var idf;
+if ('WebSocket' in window) {
+  var ws = new WebSocket("ws://192.168.0.22:8002/");
+  ws.onmessage = function (event) {
+    idf = event.data; // Récupération de l'identifiant
+  };
+}
 
-  L.control.locate().addTo(mymap);
-
-
-  if('WebSocket' in window) {
-    var ws = new WebSocket("ws://192.168.0.22:8002/");
-    // Indication de l'état
-    // var rs = document.getElementById('rs');
-    // //Lors de l'ouverture de connexion
-    // // Attente obligatoire de la connexion avant envoi de message
-    ws.onopen; // = function() {
-    //     rs.innerHTML = this.readyState;
-    //   };
-      ws.onmessage; // = function(e) {
-        // Ajout au journal du contenu du message
-        // rs.innerHTML = this.readyState;
-      // };
-  }
-
-
-// Action QD la localisation a marchée
-  function onLocationFound(e) {
-    L.marker(e.latlng).addTo(mymap).bindPopup("Problème Problème !!!").openPopup();
-    document.body.style.background = '#e74c3c';
-    // TEST ENVOI MSG TO SERVEUR
-        // TEST ENVOI MSG TO SERVEUR
-        
-            ws.send("Problème Problème !!!");
+// Envoie de la localisation en cas de problème
+function onLocationFound(e) {
+  L.marker(e.latlng).addTo(mymap).bindPopup("Problème Problème !!!").openPopup();
+  document.body.style.background = '#e74c3c';
+  // send msg to server
+  ws.send("L'enfant avec l'identifiant : " + idf + " à un problème en : " + e.latlng);
 }
 
 mymap.on('locationfound', onLocationFound);
--- parent/index.js
+++ parent/index.js
@@ -1,8 +1,12 @@
 var ws = require("nodejs-websocket");
+var idf = 0;
+
 var server = ws.createServer(function (conn) {
-	console.log("Nouvelle connexion");
+	ws.idf = idf++;
+	console.log("Nouvelle connexion : " + ws.idf);
+
 	conn.on("text", function (msg) {
-		console.log("Texte reçu : " + msg);
+		console.log("" + msg); // Reception du message du client
 	});
 	// Fermeture de connexion
 	conn.on("close", function (code, reason) {
@@ -12,6 +16,9 @@ var server = ws.createServer(function (conn) {
 	conn.on("error", function (err) {
 		console.log(err);
 	});
+
+	conn.send(ws.idf + ""); // Envoi de l'identifiant au client
+
 }).listen(8002);
 
 console.log("Serveur démarré (http://192.168.0.22:8002/) !");
